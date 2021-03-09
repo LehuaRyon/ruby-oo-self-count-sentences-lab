ba6335ebdf5a2cc0aeaa205fdc93ce59ae3ab7be
Jasmine Ryon <jryon12@priorypride.net>
2021-03-09 11:54:00 -0800

    commit tree 23b872e3cbb9d498d5e9b1f970115403efbc597d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split(/[.!?]\s/).count
+    #array.count
+    
+    # binding.pry
 
   end
 end
+
+#self.split(/[.?!]+/).grep(/\S/).count
+
+# self.gsub!(/[!?]/, “.”)
+#     sentences = self.split(“.”)
+#     sentences.delete(“”)
+#     sentences.count
+
+#return self.split(/[.+!+?+]\s/).count
